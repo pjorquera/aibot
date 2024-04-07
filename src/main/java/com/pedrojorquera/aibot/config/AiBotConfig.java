f3db3f403a4e8431b2f7f393cf708936af2344bc
Pedro Jorquera <pjorquera@okode.com>
2024-04-07 18:50:47 +0200

    Refactor & cleaning

--- src/main/java/com/pedrojorquera/aibot/config/AiBotConfig.java
+++ src/main/java/com/pedrojorquera/aibot/config/AiBotConfig.java
@@ -3,25 +3,12 @@ package com.pedrojorquera.aibot.config;
 import com.pedrojorquera.aibot.agent.AiBotAgent;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(AiBotProperties.class)
 public class AiBotConfig {
-
-    @Bean
-    ChatLanguageModel openAiChatModel(AiBotProperties properties) {
-        return OpenAiChatModel.builder()
-                .apiKey(properties.getApiKey())
-                .modelName(properties.getModel())
-                .logRequests(true)
-                .logResponses(true)
-                .build();
-    }
 
     @Bean
     public AiBotAgent agent(ChatLanguageModel chatLanguageModel) {
