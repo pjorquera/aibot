71dc91d7905a84329d4a841380970429a47380e8
Pedro Jorquera <pjorquera@okode.com>
2024-04-07 18:57:29 +0200

    Refactor & cleaning

--- src/main/java/com/pedrojorquera/aibot/agent/AiBotAgent.java
+++ src/main/java/com/pedrojorquera/aibot/agent/AiBotAgent.java
@@ -1,7 +1,9 @@
 package com.pedrojorquera.aibot.agent;
 
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.spring.AiService;
 
+@AiService
 public interface AiBotAgent {
 
     @SystemMessage({
