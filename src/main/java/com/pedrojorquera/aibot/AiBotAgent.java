5b83d259f406be5b92dd5161cca3a1955deb4b88
Pedro Jorquera <pjorquera@okode.com>
2024-04-08 21:37:26 +0200

    Refactor

--- src/main/java/com/pedrojorquera/aibot/AiBotAgent.java
+++ src/main/java/com/pedrojorquera/aibot/AiBotAgent.java
@@ -1,12 +1,44 @@
 package com.pedrojorquera.aibot;
 
 import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.service.spring.AiService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Map;
+import java.util.Scanner;
+
+@Configuration
+class Config {
+
+    @Bean
+    ChatMemory aiBotChatMemory() {
+        return MessageWindowChatMemory.withMaxMessages(30);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(AiBotAgent aiBotAgent) {
+        return args -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print("User: ");
+                String userMessage = scanner.nextLine();
+                if ("exit".equalsIgnoreCase(userMessage)) {
+                    break;
+                }
+                System.out.println("Agent: " + aiBotAgent.chat(userMessage));
+            }
+            scanner.close();
+        };
+    }
+
+}
 
 @Component
 class AiBotTools {
@@ -24,9 +56,10 @@ class AiBotTools {
     Date getCurrentTime() {
         return new Date();
     }
+
 }
 
-@AiService(tools = "aiBotTools")
+@AiService(tools = "aiBotTools", chatMemory = "aiBotChatMemory")
 public interface AiBotAgent {
     String chat(String input);
 }
